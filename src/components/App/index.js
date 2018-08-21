9b22e6507c9b2a28c1087df17f379e37f51b4961
Kiel Dehnert <kielzor@gmail.com>
2018-08-21 14:28:15 -0600

    Create helper function to start pushing into container array"

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import Header from '../Header';
 import CardContainer from '../CardContainer'
 import Button from '../Button'
 import MovieText from '../MovieText';
-// import { peopleData, planetData } from '../../helper';
+import { dataScrape } from '../../helper';
 
 
 class App extends Component {
@@ -20,35 +20,35 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    const movies = 'movies'
-    const people = 'people'
-    const planets = 'planets'
-    const vehicles = 'vehicles'
     const peopleUrl = 'https://swapi.co/api/people/'
     const planetsUrl = 'https://swapi.co/api/planets/'
     const vehiclesUrl = 'https://swapi.co/api/vehicles/'
     const movieUrl = 'https://swapi.co/api/films/'
     
 
-    // this.fetch(movies, movieUrl)
-    // this.fetch(people, peopleUrl)
-    // this.fetch(planets, planetsUrl)
-    // this.fetch(vehicles, vehiclesUrl)
+    // this.fetch(moviesUrl, 'movies')
+    // this.fetch(peopleUrl, 'people')
+    // this.fetch(planetsUrl, 'planets')
+    // this.fetch(vehiclesUrl, 'vehicles')
+    
 
     this.setState( { movies: {title: 'Return of the Jedi', 
     opening_crawl: 'Luke Skywalker has returned to his home planet of Tatooine in an attempt to rescue his friend Han Solo from the clutches of the vile gangster Jabba the Hutt. Little does Luke know that the GALACTIC EMPIRE has secretly begun construction on a new armored space',
     release_date: '1983-05-25'} })
   }
 
-  fetch = (dataType, url) => {
+  fetch = (url, dataType) => {
     fetch(url).then(response => response.json())
     .then(data => {
       console.log(dataType, data)
+      if(!dataType) return data
       if (dataType === 'movies') {
         this.setState({ [dataType]: data.results[4]})
       } else {
       this.setState({ [dataType]: data })
       }
+    dataScrape([dataType], data)
+
     })
     .catch(error => {
     })
--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,14 @@
-export const peopleData = () => {
+export const dataScrape = (type, data) => {
+  const scrapedData = []
+  if (type[0] === 'people') {
+    const fetchedData = fetch('https://swapi.co/api/planets/1/').then(response => response.json())
+    const homeworld = fetchedData
+    // const species = 'fake species'
+    // const population = 'fake pop'
 
-}
-
-export const planetData = () => {
-
+    scrapedData.push({'name': 'name'}, {'homeworld': fetchedData}, 'species', 'population')
+  } else if (type[0] === 'vehicles') {
+    scrapedData.push({'name': 'name'})
+  }
+  console.log('scrapedData', scrapedData)
 }
